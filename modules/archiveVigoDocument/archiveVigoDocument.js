80372b60f397bfebadfabbf0d3676f6d3cce2f1f
Cato Danielsen <cato.danielsen@egde.no>
2022-01-26 16:12:40 +0100

    Skriver ikke av for protokoll

--- modules/archiveVigoDocument/archiveVigoDocument.js
+++ modules/archiveVigoDocument/archiveVigoDocument.js
@@ -150,7 +150,7 @@ module.exports = async (vigoData, config) => {
 
                 const archiveRes = await p360(p360metadata, archiveOptions); // FEILIER IKKE NØDVENDIGVIS MED FEIL METADATA
 
-                if (archiveRes.Successful) {
+                if (archiveRes.Successful && documentData.documentType !== "PROTOKOLL") { //Ikke skriv av PROTOKOLL
                     const signOffData = {
                         "Document": archiveRes.DocumentNumber,
                         "Note": null,
