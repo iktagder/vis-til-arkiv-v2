9b5f8d07be4324a22d8aefe3788b24f8111d6051
Cato Danielsen <cato.danielsen@egde.no>
2022-02-03 12:01:55 +0100

    Avskriver ikke lenger utgående dokumenter

--- modules/archiveVigoDocument/archiveVigoDocument.js
+++ modules/archiveVigoDocument/archiveVigoDocument.js
@@ -160,7 +160,14 @@ module.exports = async (vigoData, config) => {
                     stats.imported++;
                     arkiveringsresultat.push(arkiveringStatusData);
 
-                    if (documentData.documentType !== "PROTOKOLL") {
+                    /*
+                        PROTOKOLL - skal ikke avskrives pga årsaker
+                        Dokument ut skal ikke avskrives pga åpenbare årsaker
+                        Basert på Konfigurasjon av dokumenttyper.xlsx som finnes i RIM2022-teams
+                    */
+                    if (documentData.documentType !== "PROTOKOLL" ||// krever oppfølging
+                        documentData.documentType !== "OPPNEMD" ||  // utgående dokument
+                        documentData.documentType !== "TILBUD") {   // utgående dokument
                         const signOffData = {
                             "Document": archiveRes.DocumentNumber,
                             "Note": null,
