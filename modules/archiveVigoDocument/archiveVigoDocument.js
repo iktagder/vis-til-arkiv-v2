397c24ff8188f489320cefd6d57929234da37e93
JahnRoger <78013538+JahnRoger@users.noreply.github.com>
2022-05-20 08:48:31 +0200

    trim på first, last name og p360 ekstra sjekk på status

--- modules/archiveVigoDocument/archiveVigoDocument.js
+++ modules/archiveVigoDocument/archiveVigoDocument.js
@@ -57,8 +57,8 @@ module.exports = async (vigoData, config) => {
 
         // Update or create private person in p360
         const studentData = {
-            lastName: vigoMelding.Etternavn,
-            firstName: vigoMelding.Fornavn,
+            lastName: vigoMelding.Etternavn.trim(),
+            firstName: vigoMelding.Fornavn.trim(),
             streetAddress: settSammenAdresse(vigoMelding.FolkeRegisterAdresse),
             zipCode: vigoMelding.FolkeRegisterAdresse.Postnummmer,
             zipPlace: vigoMelding.FolkeRegisterAdresse.Poststed,
