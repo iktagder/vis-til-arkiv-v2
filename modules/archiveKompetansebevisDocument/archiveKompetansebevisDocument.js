f1940211a93142e0a8966df4786d5594aaea4a62
Cato Danielsen <cato.danielsen@egde.no>
2022-06-07 08:05:20 +0200

    Sikrer at logg blir skrevet og logger om import er tom

--- modules/archiveKompetansebevisDocument/archiveKompetansebevisDocument.js
+++ modules/archiveKompetansebevisDocument/archiveKompetansebevisDocument.js
@@ -23,86 +23,95 @@ module.exports = async (archiveMethod, config) => {
     error: 0,
     // and whatever you like statistics on, update them wherever suitable in the flow, and finally, write them to statfile with writeStat(archiveMethod.metadataSchema, stats)
   };
-  const listOfPdfs = getPdfsInFolder(archiveMethod.inputFolder);
-  const unikeSkolenavn = hentSkolenavn(listOfPdfs);
-  unikeSkolenavn.forEach((skole) => console.table(skole));
-  // TODO: velge skole og/eller begrense innlesing til n dokumenter
 
-  //mainLoop -- alle funksjonskall returnerer null ved feil
-  for (const pdf of listOfPdfs) {
-    writeLog("--- Kompetansebevis, ny fil: " + pdf + " ---");
-
-    const pdfContent = await lesPdfInnhold(pdf, archiveMethod);
-    if (!pdfContent) continue;
-
-    const documentData = await strukturerPdfInnhold(pdfContent, archiveMethod);
-    if (!documentData) continue;
-
-    // Finn student i FINT
-    const studentInfo = await hentElevinfo(
-      documentData.studentBirthnr,
-      archiveMethod,
-      pdf
-    );
-    if (!studentInfo) continue;
-    else {
-      documentData.studentName = `${studentInfo.data.navn.fornavn} ${studentInfo.data.navn.etternavn}`;
+  try {
+    const listOfPdfs = getPdfsInFolder(archiveMethod.inputFolder);
+    if (listOfPdfs.length === 0) {
+      writeLog(
+        `Ingen kompetansebevis i import-mappen ${archiveMethod.inputFolder}`
+      );
+      return;
     }
+    // TODO: velge skole og/eller begrense innlesing til n dokumenter
+    const unikeSkolenavn = hentSkolenavn(listOfPdfs);
 
-    // syncPrivatePerson -> privatePersonRecno (oppdaterer og verifiserer at personen er registrert i p360)
-    const studentRecno = await synkOgHentStudentRecno(
-      studentInfo,
-      documentData.studentBirthnr,
-      archiveMethod,
-      pdf,
-      baseP360Options
-    );
-    if (!studentRecno) continue;
+    //mainLoop -- alle funksjonskall returnerer null ved feil
+    for (const pdf of listOfPdfs) {
+      writeLog("--- Kompetansebevis, ny fil: " + pdf + " ---");
 
-    // get elevmappe and add caseNumber to documentData
-    const elevmappe = await hentElevmappe(
-      documentData.studentBirthnr,
-      archiveMethod,
-      pdf,
-      baseP360Options
-    );
-    if (!elevmappe) continue; // TODO: opprette mappe om aktuelt
+      const pdfContent = await lesPdfInnhold(pdf, archiveMethod);
+      if (!pdfContent) continue;
 
-    documentData.elevmappeCaseNumber = elevmappe.elevmappeCaseNumber;
-    documentData.elevmappeAccessGroup = elevmappe.elevmappeAccessGroup;
-    documentData.elevmappeStatus = elevmappeAccessGroup.elevmappeStatus;
+      const documentData = await strukturerPdfInnhold(
+        pdfContent,
+        archiveMethod
+      );
+      if (!documentData) continue;
 
-    if (archiveMethod.createDocument) {
-      if (documentData.elevmappeStatus === "Avsluttet") {
-        meldFeil(
-          {},
-          `Kan ikke lagre til avsluttet mappe nr ${documentData.elevmappeCaseNumber}`,
-          archiveMethod,
-          pdf
-        );
-        continue;
+      // Finn student i FINT
+      const studentInfo = await hentElevinfo(
+        documentData.studentBirthnr,
+        archiveMethod,
+        pdf
+      );
+      if (!studentInfo) continue;
+      else {
+        documentData.studentName = `${studentInfo.navn.fornavn} ${studentInfo.navn.etternavn}`;
       }
 
-      const metadata = genererMetadata(documentData, pdf, archiveMethod);
-      if (!metadata) continue;
-      const arkivnummer = arkiverDokument(
-        metadata,
+      // syncPrivatePerson -> privatePersonRecno (oppdaterer og verifiserer at personen er registrert i p360)
+      const studentRecno = await synkOgHentStudentRecno(
+        studentInfo,
+        documentData.studentBirthnr,
         archiveMethod,
         pdf,
         baseP360Options
       );
-      if (arkivnummer) {
-        writeLog(
-          `Document archived with documentNumber ${archiveRes.DocumentNumber}`
-        );
-      }
-    }
-  } //
+      if (!studentRecno) continue;
 
-  // write statistics
-  try {
-    await writeStat(archiveMethod.metadataSchema, stats);
+      // get elevmappe and add caseNumber to documentData
+      const elevmappe = await hentElevmappe(
+        documentData.studentBirthnr,
+        archiveMethod,
+        pdf,
+        baseP360Options
+      );
+      if (!elevmappe) continue; // TODO: opprette mappe om aktuelt
+
+      documentData.elevmappeCaseNumber = elevmappe.elevmappeCaseNumber;
+      documentData.elevmappeAccessGroup = elevmappe.elevmappeAccessGroup;
+      documentData.elevmappeStatus = elevmappeAccessGroup.elevmappeStatus;
+
+      if (archiveMethod.createDocument) {
+        if (documentData.elevmappeStatus === "Avsluttet") {
+          meldFeil(
+            {},
+            `Kan ikke lagre til avsluttet mappe nr ${documentData.elevmappeCaseNumber}`,
+            archiveMethod,
+            pdf
+          );
+          continue;
+        }
+
+        const metadata = genererMetadata(documentData, pdf, archiveMethod);
+        if (!metadata) continue;
+        const arkivnummer = arkiverDokument(
+          metadata,
+          archiveMethod,
+          pdf,
+          baseP360Options
+        );
+        if (arkivnummer) {
+          writeLog(
+            `Document archived with documentNumber ${archiveRes.DocumentNumber}`
+          );
+        }
+      }
+    } //
+    // write statistics
   } catch (error) {
     writeLog(error);
+  } finally {
+    await writeStat(archiveMethod.metadataSchema, stats);
   }
 };
